b7d65e8e1d727bb80634121e2b608bf91fec274a
Charles Sherwood <kulan876321@yahoo.com>
2025-01-27 10:19:33 -0500

    Add project files.

---
+++ Chapter2Exercise3.cpp
@@ -0,0 +1,27 @@
+/*FileName:Chapter2Exersice3.cpp
+Programmer:Charles Sherwood
+Date:1/31
+Requirements:compute the sales tax on a $95 purchase
+assuming the state sales tax is 4 percent and
+the county tax is 2 percent
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//create a floater for the Money
+	double purchase, state, county,
+		total = purchase * state * county
+
+		purchase = 95;
+
+
+		cout << ;
+
+
+
+
+
+}
