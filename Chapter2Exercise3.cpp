af17a4e456bce2dc17a856c5b94f6b21913bb546
Charles Sherwood <kulan876321@yahoo.com>
2025-02-05 15:08:23 -0500

    Yurrt

--- Chapter2Exercise3.cpp
+++ Chapter2Exercise3.cpp
@@ -1,9 +1,8 @@
 /*FileName:Chapter2Exersice3.cpp
 Programmer:Charles Sherwood
 Date:2/5
-Requirements:compute the sales tax on a $95 purchase
-assuming the state sales tax is 4 percent and
-the county tax is 2 percent
+Requirements:Let the user enter any amount of 
+money and tax.
 */
 
 #include <iostream>
@@ -13,21 +12,46 @@ using namespace std;
 int main()
 {
 	//create a floater for the Money
-	double purchase = 95;
-	double state = .04;
-	double county = .02;
-	double tax1 = purchase * state;
-	double tax2 = purchase * county;
+	double purchase{}, state{}, county{};
+
+	//Get the purchase amount
+	cout << "How much was the total of the purchase?:";
+	cin >> purchase;
+	cout << "The purchase was: $" << purchase << endl;
+
+
+	//Check how much the state tax is 
+	cout << "How much is the state tax?:";
+	cin >> state;
+
+	double Percent = state / 100;
+
+
+	cout << "The sales tax is %" << state << endl;
+	double tax1 = purchase * Percent;
+	cout << "The sales tax on the purchase was: $" << tax1 << endl;
+
+
+	//Check how much the county tax is 
+	cout << "How much is the county tax?:";
+	cin >> county;
+
+	double P2 = county / 100;
+
+	cout << "the county tax is %" << county << endl;
+	double tax2 = purchase * P2;
+	cout << "The county tax on the purchase was $" << tax2 << endl;
+
+
+	//Finally add everything together
 	double total = tax1 + tax2;
+	double final = total + purchase;
 
-	cout << "The purchase was a total of $95";
-	cout << " The state tax is 4%,and 4% of 95 is " << tax1 << "\n";
-	cout << "The county tax is 2%, and 2% of 95 is " << tax2 << "\n";
-	cout << "When you add those two together you can get the total";
+	cout << "The total tax on this purchase would be $" << total << "\n";
+	cout << "Making it a total of: $" << final << "\n";
 
 
-	cout << fixed << setprecision(2) << " The total tax is $" << total << "\n";
-
+	return 0;
 
 
 
